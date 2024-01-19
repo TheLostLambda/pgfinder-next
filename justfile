ddf9ac6cf7df7b6560a6bd976e00841880e0a23a
Brooks J Rady <b.j.rady@gmail.com>
2024-01-18 17:25:55 -0800

    Finish chemical composition parser draft

--- justfile
+++ justfile
@@ -5,5 +5,5 @@ cov:
   cargo llvm-cov --open
 
 ebnf:
-  ebnf2railroad grammar/peptidoglycan.ebnf -t PGLang
+  ebnf2railroad grammar/peptidoglycan.ebnf -t PGLang --write-style
   firefox grammar/peptidoglycan.html
--- src/polychem/composition_parser.rs
+++ src/polychem/composition_parser.rs
@@ -4,48 +4,91 @@
 
 use nom::{
     branch::alt,
-    character::complete::{char, satisfy, u32},
-    combinator::{map, map_res, opt, recognize},
-    sequence::{delimited, pair},
-    IResult,
+    character::complete::{char, one_of, satisfy, u32},
+    combinator::{map, map_res, not, opt, recognize},
+    multi::{many0, many1},
+    sequence::{delimited, pair, preceded, tuple},
+    IResult, Parser,
 };
 
-use super::{chemical_database::ChemicalDatabase, Element, MassNumber, OffsetKind, Particle};
+use super::{
+    chemical_database::ChemicalDatabase, ChemicalComposition, Element, MassNumber, OffsetKind,
+    Particle,
+};
+
+type ParseResult<'a, O> = IResult<&'a str, O>;
 
 /// Element = uppercase , [ lowercase ] ;
-fn element_symbol(i: &str) -> IResult<&str, &str> {
+fn element_symbol(i: &str) -> ParseResult<&str> {
     recognize(pair(uppercase, opt(lowercase)))(i)
 }
 
 /// Isotope = "[" , Integer , Element , "]" ;
-fn isotope_expr(i: &str) -> IResult<&str, (MassNumber, &str)> {
-    delimited(char('['), pair(u32, element_symbol), char(']'))(i)
+fn isotope_expr(i: &str) -> ParseResult<(MassNumber, &str)> {
+    delimited(char('['), pair(count, element_symbol), char(']'))(i)
 }
 
 /// Element = uppercase , [ lowercase ] ;
-fn element<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> IResult<&'a str, Element> {
+fn element<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> ParseResult<Element> {
     map_res(element_symbol, |symbol| Element::new(db, symbol))
 }
 
 /// Isotope = "[" , Integer , Element , "]" ;
-fn isotope<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> IResult<&'a str, Element> {
+fn isotope<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> ParseResult<Element> {
     map_res(isotope_expr, |(mass_number, symbol)| {
         Element::new_isotope(db, symbol, mass_number)
     })
 }
 
 /// Particle = lowercase ;
-fn particle<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> IResult<&'a str, Particle> {
-    map_res(lowercase, |symbol| Particle::new(db, symbol.to_string()))
+fn particle<'a>(db: &'a ChemicalDatabase) -> impl FnMut(&'a str) -> ParseResult<Particle> {
+    map_res(recognize(lowercase), |symbol| Particle::new(db, symbol))
 }
 
 /// Offset Kind = "+" | "-" ;
-fn offset_kind(i: &str) -> IResult<&str, OffsetKind> {
-    map(alt((char('+'), char('-'))), |c| match c {
+fn offset_kind(i: &str) -> ParseResult<OffsetKind> {
+    map(one_of("+-"), |c| match c {
         '+' => OffsetKind::Add,
         '-' => OffsetKind::Remove,
         _ => unreachable!(),
     })(i)
+}
+
+// FIXME: Turn these u32's into `Count` types?
+/// Count = digit - "0" , { digit } ;
+fn count(i: &str) -> ParseResult<u32> {
+    preceded(not(char('0')), u32)(i)
+}
+
+/// Particle Offset = Offset Kind , [ Integer ] ,
+///   Particle ;
+fn particle_offset<'a>(
+    db: &'a ChemicalDatabase,
+) -> impl FnMut(&'a str) -> ParseResult<(OffsetKind, u32, Particle)> {
+    let optional_count = opt(count).map(|o| o.unwrap_or(1));
+    tuple((offset_kind, optional_count, particle(db)))
+}
+
+/// Atomic Offset = ( Element | Isotope ) , [ Count ] ;
+fn atomic_offset<'a>(
+    db: &'a ChemicalDatabase,
+) -> impl FnMut(&'a str) -> ParseResult<(Element, u32)> {
+    let optional_count = opt(count).map(|o| o.unwrap_or(1));
+    pair(alt((element(db), isotope(db))), optional_count)
+}
+
+/// Chemical Composition = { Atomic Offset }- ,
+///   { Particle Offset } ;
+fn chemical_composition<'a>(
+    db: &'a ChemicalDatabase,
+) -> impl FnMut(&'a str) -> ParseResult<ChemicalComposition> {
+    let parts = pair(many1(atomic_offset(db)), many0(particle_offset(db)));
+    map(parts, |(chemical_formula, charged_particles)| {
+        ChemicalComposition {
+            chemical_formula,
+            charged_particles,
+        }
+    })
 }
 
 /// uppercase
@@ -70,6 +113,7 @@ fn lowercase(i: &str) -> IResult<&str, char> {
 
 #[cfg(test)]
 mod tests {
+    use insta::assert_debug_snapshot;
     use miette::Result;
     use once_cell::sync::Lazy;
 
@@ -110,6 +154,27 @@ mod tests {
         assert_eq!(lowercase("hg"), Ok(("g", 'h')));
         assert_eq!(lowercase("hG"), Ok(("G", 'h')));
         // FIXME: Add a check for a richer error message when parsing fails
+        Ok(())
+    }
+
+    #[test]
+    fn test_count() -> Result<()> {
+        // Valid Counts
+        assert_eq!(count("1"), Ok(("", 1)));
+        assert_eq!(count("10"), Ok(("", 10)));
+        assert_eq!(count("422"), Ok(("", 422)));
+        assert_eq!(count("9999"), Ok(("", 9999)));
+        // Invalid Counts
+        assert!(count("0").is_err());
+        assert!(count("01").is_err());
+        assert!(count("00145").is_err());
+        assert!(count("H").is_err());
+        assert!(count("p").is_err());
+        assert!(count("+H").is_err());
+        assert!(count("[H]").is_err());
+        // Multiple Counts
+        assert_eq!(count("1OH"), Ok(("OH", 1)));
+        assert_eq!(count("42HeH"), Ok(("HeH", 42)));
         Ok(())
     }
 
@@ -259,6 +324,168 @@ mod tests {
         // Multiple Offset Kinds
         assert_eq!(offset_kind("+-"), Ok(("-", OffsetKind::Add)));
         assert_eq!(offset_kind("--"), Ok(("-", OffsetKind::Remove)));
+        Ok(())
+    }
+
+    #[test]
+    fn test_particle_offset() -> Result<()> {
+        let mut particle_offset = particle_offset(&DB);
+        macro_rules! assert_particle_offset {
+            ($input:literal, $output:literal, $kind:expr, $count:literal, $name:literal ) => {
+                assert_eq!(
+                    particle_offset($input).map(|(r, (k, c, p))| (r, (k, c, p.name))),
+                    Ok(($output, ($kind, $count, $name.to_string())))
+                );
+            };
+        }
+        // Valid Particle Offsets
+        assert_particle_offset!("+p", "", OffsetKind::Add, 1, "Proton");
+        assert_particle_offset!("-p", "", OffsetKind::Remove, 1, "Proton");
+        assert_particle_offset!("+e", "", OffsetKind::Add, 1, "Electron");
+        assert_particle_offset!("-e", "", OffsetKind::Remove, 1, "Electron");
+
+        assert_particle_offset!("+5p", "", OffsetKind::Add, 5, "Proton");
+        assert_particle_offset!("-5p", "", OffsetKind::Remove, 5, "Proton");
+        assert_particle_offset!("+5e", "", OffsetKind::Add, 5, "Electron");
+        assert_particle_offset!("-5e", "", OffsetKind::Remove, 5, "Electron");
+
+        assert_particle_offset!("+100p", "", OffsetKind::Add, 100, "Proton");
+        assert_particle_offset!("-100p", "", OffsetKind::Remove, 100, "Proton");
+        assert_particle_offset!("+100e", "", OffsetKind::Add, 100, "Electron");
+        assert_particle_offset!("-100e", "", OffsetKind::Remove, 100, "Electron");
+        // Invalid Particle Offsets
+        assert!(particle_offset("P").is_err());
+        assert!(particle_offset("Ep").is_err());
+        assert!(particle_offset("1p").is_err());
+        assert!(particle_offset("1+p").is_err());
+        assert!(particle_offset("p").is_err());
+        assert!(particle_offset("+0p").is_err());
+        assert!(particle_offset("-02e").is_err());
+        assert!(particle_offset("+-e").is_err());
+        assert!(particle_offset("+42").is_err());
+        assert!(particle_offset("-[p]").is_err());
+        // Non-Existent Particle Offsets
+        assert!(particle_offset("+m").is_err());
+        assert!(particle_offset("-3g").is_err());
+        // Multiple Particle Offsets
+        assert_particle_offset!("+3ep", "p", OffsetKind::Add, 3, "Electron");
+        assert_particle_offset!("-pe", "e", OffsetKind::Remove, 1, "Proton");
+        Ok(())
+    }
+
+    #[test]
+    fn test_atomic_offset() -> Result<()> {
+        let mut atomic_offset = atomic_offset(&DB);
+        macro_rules! assert_atomic_offset {
+            ($input:literal, $output:literal, $name:literal, $count:literal) => {
+                assert_eq!(
+                    atomic_offset($input).map(|(r, (e, c))| {
+                        let name = if let Some(a) = e.mass_number {
+                            format!("{}-{a}", e.name)
+                        } else {
+                            e.name
+                        };
+                        (r, (name, c))
+                    }),
+                    Ok(($output, ($name.to_string(), $count)))
+                );
+            };
+        }
+        // Valid Atomic Offsets
+        assert_atomic_offset!("H", "", "Hydrogen", 1);
+        assert_atomic_offset!("He", "", "Helium", 1);
+        assert_atomic_offset!("H2", "", "Hydrogen", 2);
+        assert_atomic_offset!("C18", "", "Carbon", 18);
+        assert_atomic_offset!("[1H]", "", "Hydrogen-1", 1);
+        assert_atomic_offset!("[18O]", "", "Oxygen-18", 1);
+        assert_atomic_offset!("[37Cl]", "", "Chlorine-37", 1);
+        assert_atomic_offset!("[1H]2", "", "Hydrogen-1", 2);
+        assert_atomic_offset!("[18O]3", "", "Oxygen-18", 3);
+        assert_atomic_offset!("[37Cl]5", "", "Chlorine-37", 5);
+        // Invalid Atomic Offsets
+        assert!(atomic_offset("p").is_err());
+        assert!(atomic_offset("-2p").is_err());
+        assert!(atomic_offset("ep").is_err());
+        assert!(atomic_offset("1H").is_err());
+        assert!(atomic_offset("+H").is_err());
+        assert!(atomic_offset("2[2H]").is_err());
+        assert!(atomic_offset("[H]").is_err());
+        assert!(atomic_offset("[H2]").is_err());
+        assert!(atomic_offset("[18OH]").is_err());
+        assert!(atomic_offset("[18]").is_err());
+        assert!(atomic_offset("[[18O]]").is_err());
+        assert!(atomic_offset("[-18O]").is_err());
+        assert!(atomic_offset("[+18O]").is_err());
+        // Non-Existent Atomic Offsets
+        assert!(atomic_offset("X2").is_err());
+        assert!(atomic_offset("To7").is_err());
+        assert!(atomic_offset("[42X]").is_err());
+        assert!(atomic_offset("[99To]2").is_err());
+        assert!(atomic_offset("[15C]4").is_err());
+        assert!(atomic_offset("[100Tc]8").is_err());
+        // Multiple Atomic Offsets
+        assert_atomic_offset!("OH", "H", "Oxygen", 1);
+        assert_atomic_offset!("HeH", "H", "Helium", 1);
+        assert_atomic_offset!("H2O", "O", "Hydrogen", 2);
+        assert_atomic_offset!("CO2", "O2", "Carbon", 1);
+        assert_atomic_offset!("[13C]6O2", "O2", "Carbon-13", 6);
+        assert_atomic_offset!("[3He]1H", "H", "Helium-3", 1);
+        Ok(())
+    }
+
+    #[test]
+    fn test_chemical_composition() -> Result<()> {
+        let mut chemical_composition = chemical_composition(&DB);
+        macro_rules! check_composition_snapshot {
+            ($input:literal, $output:literal) => {
+                let (
+                    rest,
+                    ChemicalComposition {
+                        chemical_formula,
+                        charged_particles,
+                    },
+                ) = chemical_composition($input).unwrap();
+                assert_eq!(rest, $output);
+                let chemical_formula: Vec<_> = chemical_formula
+                    .iter()
+                    .map(|(e, c)| {
+                        let name = if let Some(a) = e.mass_number {
+                            format!("{}-{a}", e.name)
+                        } else {
+                            e.name.clone()
+                        };
+                        (name, c)
+                    })
+                    .collect();
+                let charged_particles: Vec<_> = charged_particles
+                    .iter()
+                    .map(|(k, c, p)| (k, c, &p.name))
+                    .collect();
+                assert_debug_snapshot!((chemical_formula, charged_particles));
+            };
+        }
+        // Valid Chemical Compositions
+        check_composition_snapshot!("H2O", "");
+        check_composition_snapshot!("C11H12N2O2", "");
+        check_composition_snapshot!("OH+e", "");
+        check_composition_snapshot!("Na-e", "");
+        check_composition_snapshot!("NH3+p", "");
+        check_composition_snapshot!("Cr2O7+2e", "");
+        check_composition_snapshot!("NH2+2p+e", "");
+        check_composition_snapshot!("D2O", "");
+        check_composition_snapshot!("[2H]2O", "");
+        check_composition_snapshot!("[37Cl]5-2p", "");
+        check_composition_snapshot!("NH2[99Tc]", "");
+        // Invalid Chemical Compositions
+        assert!(chemical_composition("-2p").is_err());
+        assert!(chemical_composition("+H").is_err());
+        assert!(chemical_composition("2[2H]").is_err());
+        assert!(chemical_composition("[H+p]O").is_err());
+        // Multiple Chemical Compositions
+        check_composition_snapshot!("[37Cl]5-2p+10", "+10");
+        check_composition_snapshot!("[2H]2O+H2O", "+H2O");
+        check_composition_snapshot!("NH2[100Tc]", "[100Tc]");
+        check_composition_snapshot!("C11H12N2O2 H2O", " H2O");
         Ok(())
     }
 }
