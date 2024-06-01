f0b210da7c4ed32420cca565b53cc32dbb58e298
Brooks J Rady <b.j.rady@gmail.com>
2024-06-01 19:43:48 +0100

    perf(muropeptide): bench split parsing & building

--- crates/muropeptide/benches/api.rs
+++ crates/muropeptide/benches/api.rs
@@ -1,6 +1,6 @@
 use divan::AllocProfiler;
 use once_cell::sync::Lazy;
-use polychem::{atoms::atomic_database, AtomicDatabase, PolymerDatabase};
+use polychem::{atoms::atomic_database, AtomicDatabase, PolymerDatabase, Polymerizer};
 
 #[global_allocator]
 static ALLOC: AllocProfiler = AllocProfiler::system();
@@ -8,18 +8,40 @@ static ALLOC: AllocProfiler = AllocProfiler::system();
 const ATOMIC_KDL: &str = atomic_database::DEFAULT_KDL;
 const POLYMER_KDL: &str = include_str!("../data/polymer_database.kdl");
 const AMINO_ACIDS: [&str; 3] = ["D(Am)", "E(Am)", "J(Am)"];
+const MONOMERS: [&str; 6] = [
+    "m",
+    "gmgmgmgmgm",
+    "A",
+    "AEJAAEJAAEJAAEJAAEJA",
+    "m-A",
+    "gmgmgmgmgm-AEJAAEJAAEJAAEJAAEJA",
+];
 
 static ATOMIC_DB: Lazy<AtomicDatabase> = Lazy::new(AtomicDatabase::default);
-
 static POLYMER_DB: Lazy<PolymerDatabase> =
     Lazy::new(|| PolymerDatabase::new(&ATOMIC_DB, "polymer_database.kdl", POLYMER_KDL).unwrap());
+
+static POLYMERIZER: Lazy<Polymerizer> = Lazy::new(|| Polymerizer::new(&ATOMIC_DB, &POLYMER_DB));
 
 fn main() {
     Lazy::force(&ATOMIC_DB);
     Lazy::force(&POLYMER_DB);
+    Lazy::force(&POLYMERIZER);
     divan::main();
 }
 
 mod polymers {
+    use muropeptide::Muropeptide;
+
     // TODO: Add some benchmarks here!
+    // FIXME: Very messy and temporary!
+    use super::*;
+
+    #[divan::bench]
+    fn build_muropeptides<'a, 'p>() -> Vec<Muropeptide<'a, 'p>> {
+        MONOMERS
+            .iter()
+            .map(|structure| Muropeptide::new(&POLYMERIZER, structure).unwrap())
+            .collect()
+    }
 }
