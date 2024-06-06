d049a0270f8e0eb9ce7ea1ce9ba6562b52f15786
Brooks J Rady <b.j.rady@gmail.com>
2024-06-06 12:57:26 +0100

    feat(offset_kind): add `Not` impl

--- crates/polychem/src/atoms/offset_kind.rs
+++ crates/polychem/src/atoms/offset_kind.rs
@@ -1,5 +1,5 @@
 use std::fmt::{self, Display, Formatter};
-use std::ops::Neg;
+use std::ops::{Neg, Not};
 
 use crate::OffsetKind;
 
@@ -8,6 +8,17 @@ impl OffsetKind {
         match self {
             Self::Add => value,
             Self::Remove => -value,
+        }
+    }
+}
+
+impl Not for OffsetKind {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Self::Add => Self::Remove,
+            Self::Remove => Self::Add,
         }
     }
 }
@@ -39,6 +50,14 @@ mod tests {
         assert_eq!(remove.offset(1), -1);
         assert_eq!(add.offset(dec!(3.14)), dec!(3.14));
         assert_eq!(remove.offset(dec!(3.14)), dec!(-3.14));
+    }
+
+    #[test]
+    fn not() {
+        let add = OffsetKind::Add;
+        let remove = OffsetKind::Remove;
+        assert_eq!(!add, remove);
+        assert_eq!(!remove, add);
     }
 
     #[test]
