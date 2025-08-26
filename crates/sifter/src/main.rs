37e5a00cb391a426f308ca3e154a7880aad77f67
Brooks J Rady <b.j.rady@gmail.com>
2025-08-26 20:49:22 +0100

    wip(sifter): dump all MS2 scan information
    Closes #29

--- crates/sifter/src/main.rs
+++ crates/sifter/src/main.rs
@@ -1,3 +1,64 @@
+use std::io;
+
+use mzdata::{prelude::*, spectrum::SpectrumDescription};
+
+// REMEMBER: I'm prototyping! Code will be re-written afterwards!
+
+#[derive(Debug)]
+struct Ms2Scan {
+    index: usize,
+    start_time: f64,
+    precursor_mz: f64,
+}
+
+// NOTE: It's a private function, and there's less boilerplate when taking by value
+#[expect(clippy::needless_pass_by_value)]
+fn fetch_ms2_precursor(spectrum: impl SpectrumLike) -> Option<Ms2Scan> {
+    let SpectrumDescription {
+        index,
+        ms_level,
+        acquisition,
+        precursor,
+        ..
+    } = spectrum.description();
+
+    (*ms_level == 2).then(|| {
+        let precursor_ions = precursor
+            .as_ref()
+            .expect("MS2 scan was missing precursor ion information")
+            .ions
+            .as_slice();
+        assert_eq!(
+            precursor_ions.len(),
+            1,
+            "multiple precursor ions are not yet supported"
+        );
+
+        Ms2Scan {
+            index: index + 1,
+            start_time: acquisition.start_time(),
+            precursor_mz: precursor_ions[0].mz,
+        }
+    })
+}
+
+// FIXME: I should use the writing functionality of this crate to generate some smaller test files!!!
+fn mz_read_path() -> io::Result<()> {
+    // FIXME: `mz_read!` should probably add that `.as_ref()` itself...
+    let ms2_scans: Vec<_> = mzdata::mz_read!(
+        "tests/data/WT.mzML.gz".as_ref(),
+        reader => {
+            reader.filter_map(fetch_ms2_precursor).collect()
+    })?;
+
+    let start_index = 2280;
+    let length = 5;
+    dbg!(&ms2_scans[start_index..start_index + length]);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting file read...");
+    mz_read_path().unwrap();
 }
