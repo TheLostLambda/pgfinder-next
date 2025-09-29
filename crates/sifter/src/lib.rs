f7dc9fa1d92bd014314717a70e98a6909ff221a3
Brooks J Rady <b.j.rady@gmail.com>
2025-09-29 16:14:59 +0100

    feat(sifter): add and test `ppm_window`

--- crates/sifter/src/lib.rs
+++ crates/sifter/src/lib.rs
@@ -1,20 +1,20 @@
-use std::io::{Cursor, Read};
+use std::io::Cursor;
 
 use polars::{error::PolarsResult, frame::DataFrame, io::SerReader, prelude::CsvReader};
-
-fn load_pgfinder_output(csv: impl Read) -> PolarsResult<DataFrame> {
-    let col_structure = "Structure";
-    let col_theo = "Consolidated Theo(Da)";
-    todo!()
-}
 
 fn load_mass_database(csv: &str) -> PolarsResult<DataFrame> {
     let csv_reader = Cursor::new(csv);
     CsvReader::new(csv_reader).finish()
 }
 
+fn ppm_window(mz: f64, ppm: f64) -> (f64, f64) {
+    let half_window = mz * ppm / 1_000_000.;
+    (mz - half_window, mz + half_window)
+}
+
 #[cfg(test)]
 mod tests {
+    use assert_float_eq::assert_float_absolute_eq;
     use polars::prelude::DataType;
 
     use super::*;
@@ -32,6 +32,21 @@ mod tests {
             mass_database.dtypes(),
             &[DataType::String, DataType::Float64]
         );
-        assert_eq!(mass_database.height(), 21)
+        assert_eq!(mass_database.height(), 21);
+    }
+
+    #[test]
+    fn test_ppm_window() {
+        let (min, max) = ppm_window(941.407_703, 1.0);
+        assert_float_absolute_eq!(min, 941.406_762);
+        assert_float_absolute_eq!(max, 941.408_644);
+
+        let (min, max) = ppm_window(941.407_703, 7.5);
+        assert_float_absolute_eq!(min, 941.400_642);
+        assert_float_absolute_eq!(max, 941.414_764);
+
+        let (min, max) = ppm_window(471.711_128, 10.0);
+        assert_float_absolute_eq!(min, 471.706_411);
+        assert_float_absolute_eq!(max, 471.715_845);
     }
 }
