ddbce291b5209a4d7a570b3a4ecea33a91f5f12a
Brooks J Rady <b.j.rady@gmail.com>
2025-10-10 14:43:00 +0100

    feat(sifter): add `Mz` newtype
    Gives floating-point types reliable `Eq` and `Ord` implementations.

--- crates/sifter/src/lib.rs
+++ crates/sifter/src/lib.rs
@@ -1,5 +1,6 @@
-use std::io::Cursor;
+use std::{cmp::Ordering, io::Cursor};
 
+use derive_more::{From, Into};
 use polars::{error::PolarsResult, frame::DataFrame, io::SerReader, prelude::CsvReader};
 
 fn load_mass_database(csv: &str) -> PolarsResult<DataFrame> {
@@ -11,6 +12,33 @@ fn ppm_window(mz: f64, ppm: f64) -> (f64, f64) {
     let half_window = mz * ppm / 1_000_000.;
     (mz - half_window, mz + half_window)
 }
+
+// PERF: Try out `f32` and see if that saves enough space to speed things up!
+#[derive(Copy, Clone, From, Into)]
+struct Mz(f64);
+
+impl Ord for Mz {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.total_cmp(&other.0)
+    }
+}
+
+impl PartialOrd for Mz {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Mz {
+    fn eq(&self, other: &Self) -> bool {
+        // NOTE: We must use the `.cmp()` from `Ord` here — the results of `.total_cmp()` do *not* match the default
+        // implementations of `PartialEq` for floats. The `.total_cmp()` method treats `-0` and `0` as different, but
+        // the default `PartialEq` for floats does not!
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl Eq for Mz {}
 
 #[cfg(test)]
 mod tests {
@@ -33,6 +61,20 @@ mod tests {
             &[DataType::String, DataType::Float64]
         );
         assert_eq!(mass_database.height(), 21);
+    }
+
+    #[test]
+    fn mz_traits() {
+        // Test `From` and `Into` impls
+        let a: Mz = (941.407_703).into();
+        let b = Mz::from(471.711_128);
+        assert_float_absolute_eq!(a.into(), 941.407_703);
+        assert_float_absolute_eq!(b.into(), 471.711_128);
+        // Test `Copy` and `Ord` impl
+        assert_eq!(a.cmp(&b), Ordering::Greater);
+        assert_eq!(b.cmp(&a), Ordering::Less);
+        assert_eq!(a.cmp(&a), Ordering::Equal);
+        assert_eq!(b.cmp(&b), Ordering::Equal);
     }
 
     #[test]
