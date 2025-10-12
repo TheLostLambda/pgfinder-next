f5b1b32099441fa381358f3021827056d29354e5
Brooks J Rady <b.j.rady@gmail.com>
2025-10-12 16:32:50 +0100

    refactor(sifter): `Mz` -> `TotalFloat`
    I've created a `type Mz = TotalFloat` alias, and I can do the same for
    retention time, which is another float that will need `Ord` and `Eq`
    implementations.

--- crates/sifter/src/lib.rs
+++ crates/sifter/src/lib.rs
@@ -11,28 +11,21 @@ fn load_mass_database(csv: &str) -> PolarsResult<DataFrame> {
 
 // PERF: Try out `f32` and see if that saves enough space to speed things up!
 #[derive(Copy, Clone, Debug, From, Into)]
-struct Mz(f64);
+struct TotalFloat(f64);
 
-impl Mz {
-    fn ppm_window(mz: f64, ppm: f64) -> RangeInclusive<Self> {
-        let (min_mz, max_mz) = Tolerance::PPM(ppm).bounds(mz);
-        Self(min_mz)..=Self(max_mz)
-    }
-}
-
-impl Ord for Mz {
+impl Ord for TotalFloat {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.total_cmp(&other.0)
     }
 }
 
-impl PartialOrd for Mz {
+impl PartialOrd for TotalFloat {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl PartialEq for Mz {
+impl PartialEq for TotalFloat {
     fn eq(&self, other: &Self) -> bool {
         // NOTE: We must use the `.cmp()` from `Ord` here — the results of `.total_cmp()` do *not* match the default
         // implementations of `PartialEq` for floats. The `.total_cmp()` method treats `-0` and `0` as different, but
@@ -41,7 +34,16 @@ impl PartialEq for Mz {
     }
 }
 
-impl Eq for Mz {}
+impl Eq for TotalFloat {}
+
+type Mz = TotalFloat;
+
+impl Mz {
+    fn ppm_window(mz: f64, ppm: f64) -> RangeInclusive<Self> {
+        let (min_mz, max_mz) = Tolerance::PPM(ppm).bounds(mz);
+        Self(min_mz)..=Self(max_mz)
+    }
+}
 
 // PERF: After getting some benchmarks in place, try cutting the size of this struct in half (f64 → f32) — the 64 bits
 // definitely isn't needed precision-wise, so if it helps performance, it's probably worth the small amount of type
@@ -53,11 +55,9 @@ struct ScanKey {
 }
 
 impl ScanKey {
-    // MISSING: It's not public API, so `const` is unnecessary here
-    #[expect(clippy::missing_const_for_fn)]
     fn new(precursor: f64, scan_number: usize) -> Self {
         Self {
-            precursor: Mz(precursor),
+            precursor: Mz::from(precursor),
             scan_number,
         }
     }
@@ -122,29 +122,29 @@ mod tests {
     #[test]
     fn mz_ppm_window() {
         let window = Mz::ppm_window(941.407_703, 7.5);
-        let Bound::Included(&Mz(start)) = window.start_bound() else {
+        let Bound::Included(&TotalFloat(start)) = window.start_bound() else {
             panic!("expected a `.start_bound()` matching `Bound::Included(&Mz(_))`");
         };
-        let Bound::Included(&Mz(end)) = window.end_bound() else {
+        let Bound::Included(&TotalFloat(end)) = window.end_bound() else {
             panic!("expected an `.end_bound()` matching `Bound::Included(&Mz(_))`");
         };
         assert_float_absolute_eq!(start, 941.400_642);
         assert_float_absolute_eq!(end, 941.414_764);
-        assert!(window.contains(&Mz(941.407_703)));
+        assert!(window.contains(&Mz::from(941.407_703)));
     }
 
     #[test]
     fn scan_key_ppm_window() {
         let window = ScanKey::ppm_window(471.711_128, 10.0);
         let Bound::Included(&ScanKey {
-            precursor: Mz(start_mz),
+            precursor: TotalFloat(start_mz),
             scan_number: start_scan,
         }) = window.start_bound()
         else {
             panic!("expected a `.start_bound()` matching `Bound::Included(&Mz(_))`");
         };
         let Bound::Included(&ScanKey {
-            precursor: Mz(end_mz),
+            precursor: TotalFloat(end_mz),
             scan_number: end_scan,
         }) = window.end_bound()
         else {
@@ -169,7 +169,7 @@ mod tests {
                 942.4119,
                 942.413_513_183_594,
             ]
-            .map(Mz),
+            .map(Mz::from),
         ));
         let filter_peaks = |ppm| -> Vec<_> { peaks.filter_peaks(query, ppm).collect() };
         assert_eq!(filter_peaks(0.0), vec![]);
