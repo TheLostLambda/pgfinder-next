69be1ef3295d45e6591019cc2bdbf6c05280478f
Brooks J Rady <b.j.rady@gmail.com>
2025-10-16 15:35:15 +0100

    refactor(sifter): move `Ms2Index` to new module

--- crates/sifter/src/lib.rs
+++ crates/sifter/src/lib.rs
@@ -1,24 +1,11 @@
+mod ms2_index;
 mod peaks;
 mod scan_kv;
 mod total_float;
 
-use std::{
-    borrow::Cow,
-    collections::BTreeMap,
-    io::{Cursor, Read},
-};
+use std::io::Cursor;
 
-use flate2::read::GzDecoder;
-use mzdata::{
-    MzMLReader,
-    io::DetailLevel,
-    prelude::{IonProperties, SpectrumLike},
-    spectrum::MultiLayerSpectrum,
-};
 use polars::{error::PolarsResult, frame::DataFrame, io::SerReader, prelude::CsvReader};
-use rayon::prelude::*;
-
-use crate::scan_kv::{ScanKey, ScanValue};
 
 fn load_mass_database(csv: &str) -> PolarsResult<DataFrame> {
     let csv_reader = Cursor::new(csv);
@@ -28,80 +15,13 @@ fn load_mass_database(csv: &str) -> PolarsResult<DataFrame> {
 // FIXME: Use a better error type from `thiserror`
 type Result<T> = std::result::Result<T, &'static str>;
 
-#[derive(Clone, Eq, PartialEq, Debug)]
-pub struct Ms2Index(BTreeMap<ScanKey, ScanValue>);
-
-impl Ms2Index {
-    pub fn from_bytes(bytes: impl AsRef<[u8]>) -> Result<Self> {
-        let bytes = Self::maybe_decode_bytes(bytes.as_ref())?;
-
-        let mzml = MzMLReader::with_buffer_capacity_and_detail_level(
-            bytes.as_ref(),
-            10_000,
-            DetailLevel::Lazy,
-        );
-        let spectra: Vec<_> = mzml.collect();
-
-        Self::from_spectra(spectra)
-    }
-
-    pub fn from_spectra(
-        spectra: impl IntoParallelIterator<Item = MultiLayerSpectrum>,
-    ) -> Result<Self> {
-        spectra
-            .into_par_iter()
-            .filter(|spectrum| spectrum.ms_level() == 2)
-            .map(|mut spectrum| {
-                let precursor = spectrum
-                    .precursor()
-                    .ok_or("MS2 spectrum was missing precursor ion information")?
-                    .mz();
-                // NOTE: This is assuming that the mzML file we've been given represents a full MS run — if this file
-                // is a slice of a larger run, then the scan `.id()` will differ from this!
-                let scan_number = spectrum.index() + 1;
-                let start_time = spectrum.start_time();
-
-                let peaks = spectrum
-                    .try_build_centroids()
-                    .map_err(|_| "failed to find centroided peak data")?
-                    .into();
-
-                Ok((
-                    ScanKey::new(precursor, scan_number),
-                    ScanValue::new(start_time, peaks),
-                ))
-            })
-            .collect::<Result<_>>()
-            .map(Ms2Index)
-    }
-
-    fn maybe_decode_bytes(bytes: &[u8]) -> Result<Cow<'_, [u8]>> {
-        let mut gz_decoder = GzDecoder::new(bytes);
-        if gz_decoder.header().is_some() {
-            // NOTE: Our decompressed data should be *at least* as big as the compressed data, so we can use the size
-            // of the compressed data to allocate some initial capacity
-            let mut decoded_bytes = Vec::with_capacity(bytes.len());
-            gz_decoder
-                .read_to_end(&mut decoded_bytes)
-                .map_err(|_| "failed to decompress bytes")?;
-            Ok(Cow::Owned(decoded_bytes))
-        } else {
-            Ok(Cow::Borrowed(bytes))
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use insta::assert_debug_snapshot;
-    use mzdata::{MzMLReader, io::DetailLevel};
     use polars::prelude::DataType;
 
     use super::*;
 
     const MASS_DATABASE_CSV: &str = include_str!("../tests/data/Exchange.csv");
-    const MZML: &[u8] = include_bytes!("../tests/data/WT (6.7–7.3 min).mzML");
-    const MZML_GZ: &[u8] = include_bytes!("../tests/data/WT (6.7–7.3 min).mzML.gz");
 
     #[test]
     fn test_load_mass_database() {
@@ -115,19 +35,5 @@ mod tests {
             &[DataType::String, DataType::Float64]
         );
         assert_eq!(mass_database.height(), 21);
-    }
-
-    #[test]
-    fn ms2_index_from() {
-        let mzml =
-            MzMLReader::with_buffer_capacity_and_detail_level(MZML, 10_000, DetailLevel::Lazy);
-        let spectra: Vec<_> = mzml.collect();
-        let ms2_index = Ms2Index::from_spectra(spectra).unwrap();
-        assert_debug_snapshot!(ms2_index);
-
-        let from_bytes = Ms2Index::from_bytes(MZML).unwrap();
-        assert_eq!(from_bytes, ms2_index);
-        let from_gzipped_bytes = Ms2Index::from_bytes(MZML_GZ).unwrap();
-        assert_eq!(from_gzipped_bytes, ms2_index);
     }
 }
---
+++ crates/sifter/src/ms2_index.rs
@@ -0,0 +1,119 @@
+// Standard Library Imports
+use std::{borrow::Cow, collections::BTreeMap, io::Read};
+
+// External Crate Imports
+use flate2::read::GzDecoder;
+use mzdata::{
+    MzMLReader,
+    io::DetailLevel,
+    prelude::{IonProperties, SpectrumLike},
+    spectrum::MultiLayerSpectrum,
+};
+use rayon::prelude::*;
+
+// Local Crate Imports
+use crate::{
+    Result,
+    scan_kv::{ScanKey, ScanValue},
+};
+
+// Public API ==========================================================================================================
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Ms2Index(BTreeMap<ScanKey, ScanValue>);
+
+impl Ms2Index {
+    pub fn from_bytes(bytes: impl AsRef<[u8]>) -> Result<Self> {
+        let bytes = Self::decode_bytes_if_compressed(bytes.as_ref())?;
+
+        // FIXME: I think `mzdata` should have a way to construct an `MzMLReader` *without* a buffer! For now, the
+        // buffer capacity is `10,000` bytes, completely arbitrarily
+        let mzml = MzMLReader::with_buffer_capacity_and_detail_level(
+            bytes.as_ref(),
+            10_000,
+            DetailLevel::Lazy,
+        );
+
+        // TODO: Benchmark if this `.collect()` is faster or slower than `.par_bridge()`
+        let spectra: Vec<_> = mzml.collect();
+
+        Self::from_spectra(spectra)
+    }
+
+    pub fn from_spectra(
+        spectra: impl IntoParallelIterator<Item = MultiLayerSpectrum>,
+    ) -> Result<Self> {
+        spectra
+            .into_par_iter()
+            .filter(|spectrum| spectrum.ms_level() == 2)
+            .map(|mut spectrum| {
+                let precursor = spectrum
+                    .precursor()
+                    .ok_or("MS2 spectrum was missing precursor ion information")?
+                    .mz();
+                // NOTE: This is assuming that the mzML file we've been given represents a full MS run — if this file
+                // is a slice of a larger run, then the scan `.id()` will differ from this!
+                let scan_number = spectrum.index() + 1;
+                let start_time = spectrum.start_time();
+
+                let peaks = spectrum
+                    .try_build_centroids()
+                    .map_err(|_| "failed to find centroided peak data")?
+                    .into();
+
+                Ok((
+                    ScanKey::new(precursor, scan_number),
+                    ScanValue::new(start_time, peaks),
+                ))
+            })
+            .collect::<Result<_>>()
+            .map(Ms2Index)
+    }
+}
+
+// Private Methods =====================================================================================================
+
+impl Ms2Index {
+    fn decode_bytes_if_compressed(bytes: &[u8]) -> Result<Cow<'_, [u8]>> {
+        let mut gz_decoder = GzDecoder::new(bytes);
+
+        if gz_decoder.header().is_some() {
+            // NOTE: Our decompressed data should be *at least* as big as the compressed data, so we can use the size
+            // of the compressed data to allocate some initial capacity
+            let mut decoded_bytes = Vec::with_capacity(bytes.len());
+            gz_decoder
+                .read_to_end(&mut decoded_bytes)
+                .map_err(|_| "failed to decompress bytes")?;
+            Ok(Cow::Owned(decoded_bytes))
+        } else {
+            Ok(Cow::Borrowed(bytes))
+        }
+    }
+}
+
+// Module Tests ========================================================================================================
+
+#[cfg(test)]
+mod tests {
+    use insta::assert_debug_snapshot;
+    use mzdata::{MzMLReader, io::DetailLevel};
+
+    use super::*;
+
+    const MZML: &[u8] = include_bytes!("../tests/data/WT (6.7–7.3 min).mzML");
+    const MZML_GZ: &[u8] = include_bytes!("../tests/data/WT (6.7–7.3 min).mzML.gz");
+
+    #[test]
+    fn ms2_index_from() {
+        let mzml =
+            MzMLReader::with_buffer_capacity_and_detail_level(MZML, 10_000, DetailLevel::Lazy);
+        let spectra: Vec<_> = mzml.collect();
+        let ms2_index = Ms2Index::from_spectra(spectra).unwrap();
+        assert_debug_snapshot!(ms2_index);
+
+        let from_bytes = Ms2Index::from_bytes(MZML).unwrap();
+        assert_eq!(from_bytes, ms2_index);
+        let from_gzipped_bytes = Ms2Index::from_bytes(MZML_GZ).unwrap();
+        assert_eq!(from_gzipped_bytes, ms2_index);
+    }
+}
